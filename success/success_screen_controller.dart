4997027278613c4cb81a3a2b1266b17a3ddf16ae
RSyunyakov <43168863+RSunyakov@users.noreply.github.com>
2022-11-10 12:51:30 +0300

    code: примеры кода

---
+++ success/success_screen_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'package:teremok/ui/router/routing.dart';
+import 'package:vfx_flutter_common/getx_helpers.dart';
+
+class SuccessScreenController extends StatexController {
+  void toMainScreen() => Get.toNamed(AppRoutes.mainScreen);
+}
